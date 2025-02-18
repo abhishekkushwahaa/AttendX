7b12332bfbb9613820461d8532343ae2bc7942e2
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2025-02-18 22:16:31 +0530

    cors problem solve

--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,40 @@
 package main
 
 import (
+	"time"
+
 	"github.com/abhishekkushwahaa/AttendX/config"
 	"github.com/abhishekkushwahaa/AttendX/db"
 	"github.com/abhishekkushwahaa/AttendX/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	config.LoadEnv()
+
 	db.InitDB()
 
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	r.OPTIONS("/*path", func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "http://localhost:5173")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Status(204)
+	})
+
 	routes.RegisterRoutes(r)
+
 	r.Run(":8081")
 }
